94b9d80b8cc80fa3783751bf472b84e701701a20
muhammed-makki <e_muhammedmakki@yahoo.com>
2022-11-10 18:53:39 +0500

    mongo db changes

--- begin-mongo/db-connection.js
+++ begin-mongo/db-connection.js
@@ -1,18 +1,35 @@
 const mongoose = require('mongoose');
-const userSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password:String,
+mongoose.connect("mongodb://localhost:27017");
+const conn = mongoose.connection;
+const emmployeeSchema = mongoose.Schema({
+    first_name: String,
+    last_name: String,
     gender: Number,
-    basicSalary: Number,
-    tax: Number,
-    salary: Number
+    city_id: Number
+}, {collection: "employee"});
+// userSchema.methods.userSalary = function(){
+//     console.log('%s take home salary is %d', this.name, (this.basicSalary - ((this.basicSalary * this.tax) / 100)));
+// }
+const employee = mongoose.model('hrm', emmployeeSchema);
+const firstEmployee = new employee({first_name: 'Muhammed', last_name: 'Makki', gender: 0, city_id: 17});
+
+conn.on('connected', function(){
+    console.log('db connected succesfully');
 });
-userSchema.methods.userSalary = function(){
-    console.log('%s take home salary is %d', this.name, (this.basicSalary - ((this.basicSalary * this.tax) / 100)));
-}
-const user = mongoose.model('user', userSchema);
-const firstUser = new user({name: 'Makki', email: 'makki@gmail.com', password:'abc123', gender: 0, basicSalary: 50000, tax: 8});
-firstUser.tax = 12;
-firstUser.userSalary();
+
+conn.on('disconnected', function(){
+    console.log('Disconnected Successfully');
+});
+
+conn.on('error', console.error.bind(console, 'error detected'));
+
+conn.once('open', function(){
+    firstEmployee.save(function(err, res) {
+        if(err) throw err;
+        console.log('res', res);
+        conn.close();
+    }, {collection: "employee"});
+})
+//firstUser.tax = 12;
+//firstUser.userSalary();
 
